df75dd45e02eb38928dc977dad31d0df13803157
BugHunter-X <askzenai@gmail.com>
2024-12-05 14:02:15 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,46 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]';
+
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(context.req, context.res, authOptions);
+  // console.log(session);
+  return {
+    props: {
+      session
+    },
+  };
+}
+
+export default function About({ session }) {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {session ? (
+        <div>
+          <p>Logged in as {session.user.email}</p>
+          <p>You are authenticated</p>
+        </div>
+      ) : (
+        <p>Not authenticated</p>
+      )}
+    </div>
+  );
+}
+```
